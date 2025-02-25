1c06a4572e787b61c22fd32a8c6a05b8bf6ef260
JRafael2025 <biaimportsml@gmail.com>
2025-02-25 10:24:53 -0300

    Adicionando next.config.js e .vercelignore para corrigir deploy

---
+++ frontend/next-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // Habilita o App Router no Next.js 13+
+  },
+};
+
+module.exports = nextConfig;
